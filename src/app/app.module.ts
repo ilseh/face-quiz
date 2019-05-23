f978ba207f9ece05e32ed5ce3bd4312464a936a5
Ilse Haanstra <ilse.haanstra@yarden.nl>
2019-05-23 22:25:31 +0200

    Added input to specify zip file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FaceCardComponent } from './face-card/face-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCardModule, MatDialogModule, MatRadioModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInput,
+  MatInputModule,
+  MatRadioModule
+} from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FaceQuizComponent } from './face-quiz/face-quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FaceCardComponent
+    FaceCardComponent,
+    FaceQuizComponent
   ],
   imports: [
     BrowserModule, BrowserAnimationsModule, MatCardModule, MatRadioModule, HttpClientModule, ReactiveFormsModule,
-    MatDialogModule, MatButtonModule
+    MatDialogModule, MatButtonModule, MatInputModule, MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
