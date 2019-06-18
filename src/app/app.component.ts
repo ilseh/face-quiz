e69f5ed68560eef04a99d6165b5d7b0e41919e0f
Ilse Haanstra <ilse.haanstra@ns.nl>
2019-06-18 22:49:36 +0200

    Yes,  display image retrieved from zip in page works.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { FormControl } from '@angular/forms';
 import * as JSZip from 'jszip';
 import * as fs from 'file-system';
 import { ZipService } from '../lib/zip/zip.service';
+import { ZipEntryInterface } from '../lib/zip/zip-entry.interface';
+// import { saveAs } from 'file-saver/FileSaver';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-root',
@@ -30,23 +33,51 @@ export class AppComponent {
 
   fileChanged(event) {
     const reader = new FileReader();
-    reader.onload = function() {
-      alert(reader.result);
-    }
+    // reader.onload = function() {
+    //   alert(reader.result);
+    // };
 
     const file = event.target.files[0];
     this.zipService.getEntries(file)
       .subscribe(zip => {
-        console.log('>>>>>>>>>>>>>>>>.zip: ', zip);
-        zip.map(zipfile => {
+        console.log('>>>>>>>>>>>>>>>>.zip length: ', zip.length);
+        zip.map((zipfile: ZipEntryInterface) => {
+          console.log('filename: ', zipfile.filename);
           const task = this.zipService.getData(zipfile);
           console.log('task>>>>>>>>>>>>>', task);
           task.data.subscribe(data => {
             console.log('>>>>>>>>>>.data: ', data);
             reader.readAsText(data);
+            // this.saveToFileSystem(zipfile.filename, data, 'image/png');
+            const blob = new Blob([data], { type: 'image/png' });
+            this.previewFile(blob);
           });
         });
       });
   }
 
+
+  private saveToFileSystem(filename: string, content: any, type: string) {
+    const blob = new Blob([content], { type: type });
+    // saveAs(blob, filename);
+    const reader = new FileReader();
+
+    reader.addEventListener('load', () => {
+      console.log('load result: ', reader.result)
+    });
+    reader.readAsDataURL(blob);
+  }
+
+  private previewFile(file: Blob) {
+    const preview = document.querySelector('img');
+    const reader  = new FileReader();
+
+    reader.addEventListener('load', function () {
+      preview.src = reader.result;
+    }, false);
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  }
 }
