058a734677d51e9f13bad2f7a29cbae92d7b9877
ilseh <ilseh@users.noreply.github.com>
2019-12-01 22:03:02 +0100

    Migrate to jszip (#2)
    * Migrating to jszip

--- src/app/services/quizstate.service.spec.ts
+++ src/app/services/quizstate.service.spec.ts
@@ -1,12 +1,37 @@
-import { TestBed } from '@angular/core/testing';
-
 import { QuizstateService } from './quizstate.service';
+import { createServiceFactory, SpectatorService, SpyObject } from '@ngneat/spectator';
+import { FaceszipService } from './faceszip.service';
+import { of } from 'rxjs';
+import { QUIZ_NUMBER_OF_ALTERNATIVES } from './quiz-helper';
 
-xdescribe('QuizstateService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe('QuizstateService', () => {
+  let spectator: SpectatorService<QuizstateService>;
+  let service: QuizstateService;
+  let facezipServiceMock: SpyObject<FaceszipService>;
+  const NAMES = ['image1.png', 'image2.png', 'image3.png', 'image4.png'];
+
+  const createService = createServiceFactory({
+    service: QuizstateService,
+    mocks: [FaceszipService]
+  });
+
+  beforeEach(() => {
+    spectator = createService();
+    service = spectator.service;
+    facezipServiceMock = spectator.get(FaceszipService);
+  });
 
   it('should be created', () => {
-    const service: QuizstateService = TestBed.get(QuizstateService);
     expect(service).toBeTruthy();
+  });
+
+  describe('newQuizItems', () => {
+    it('should return quiz items', async () => {
+      facezipServiceMock.getNames.and.returnValue(of(NAMES));
+      const result = await service.newQuizItems().toPromise() ;
+      expect(result.length).toBe(4);
+      // alternative names + name of the current item
+      result.forEach(item => expect(item.alternatives.length).toBe(QUIZ_NUMBER_OF_ALTERNATIVES + 1));
+    });
   });
 });
--- src/lib/zip/zip.service.ts
+++ src/lib/zip/zip.service.ts
@@ -1,48 +1,47 @@
 // This is added globally by the zip.js library
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { from, Observable, of, Subject } from 'rxjs';
 import { ZipEntryInterface } from './zip-entry.interface';
 import { ZipTaskInterface } from './zip-task.interface';
 import { ZipTaskProgressInterface } from './zip-task-progress.interface';
+
+import JSZip, { JSZipObject } from 'jszip';
+import { fromPromise } from 'rxjs/internal-compatibility';
+import { map } from 'rxjs/operators';
 
 declare const zip: any;
 
 @Injectable()
 export class ZipService {
+  private ziptest = new JSZip();
 
   constructor() {
-    zip.workerScriptsPath = 'scripts/';
+    // zip.workerScriptsPath = 'scripts/';
+
   }
 
-  getEntries(file): Observable<Array<ZipEntryInterface>> {
-    return new Observable(subscriber => {
-      const reader = new zip.BlobReader(file);
-      zip.createReader(reader, zipReader => {
-        zipReader.getEntries(entries => {
-          subscriber.next(entries);
-          subscriber.complete();
-        });
-      }, message => {
-        subscriber.error({ message });
-      });
-    });
+  getEntries(fileName): Observable<Array<JSZipObject>> {
+    return fromPromise(this.ziptest.loadAsync(fileName)).pipe(map((jsZip: JSZip) => {
+      const files: Array<JSZipObject> = [];
+
+      // tslint:disable-next-line:forin
+      for (fileName in jsZip.files) {
+        files.push(jsZip.files[fileName]);
+      }
+      return files;
+    }));
   }
 
-  getData(entry: ZipEntryInterface): ZipTaskInterface {
-    const progress = new Subject<ZipTaskProgressInterface>();
-    const data = new Observable<Blob>(subscriber => {
-      const writer = new zip.BlobWriter();
-
-      // Using `as any` because we don't want to expose this
-      // method in the interface
-      (entry as any).getData(writer, blob => {
-        subscriber.next(blob);
-        subscriber.complete();
-        progress.next(null);
-      }, (current, total) => {
-        progress.next({ active: true, current, total });
-      });
+  getData(entry: JSZipObject): {progress: Subject<number>, data: Subject<BlobPart>} {
+    const progress$ = new Subject<number>();
+    const data$ = new Subject<BlobPart>();
+    entry.async('blob', function updateCallback(metadata) {
+      console.log('progression: ' + metadata.percent.toFixed(2) + ' %');
+      progress$.next((metadata.percent));
+    }).then(function (blob) {
+      console.log('data received');
+      data$.next(blob);
     });
-    return { progress, data };
+    return {progress: progress$, data: data$};
   }
 }
