bb4e478ca1933c21c2eb2eaf3f4e04d9579e0890
Ilse Haanstra <ilse.haanstra@ns.nl>
2019-12-01 20:24:28 +0100

    Fix of test

--- src/app/services/quizstate.service.spec.ts
+++ src/app/services/quizstate.service.spec.ts
@@ -31,7 +31,7 @@ describe('QuizstateService', () => {
   describe('newQuizItems', () => {
     it('should return quiz items', async () => {
       facezipServiceMock.getNames.and.returnValue(of(NAMES));
-      const result = await service.newQuizItems();
+      const result = await service.newQuizItems().toPromise();
       expect(result.length).toBe(4);
       // alternative names + name of the current item
       result.forEach(item => expect(item.alternatives.length).toBe(QUIZ_NUMBER_OF_ALTERNATIVES + 1))
